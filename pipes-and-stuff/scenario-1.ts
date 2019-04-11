1e578f9304fe35160c8c395dc8803d2dc07da854
João Seixas <up201505648@fe.up.pt>
2019-04-11 18:03:00 +0100

    little update on scenario 1

--- pipes-and-stuff/scenario-1.ts
+++ pipes-and-stuff/scenario-1.ts
@@ -14,10 +14,16 @@ class AsyncSemaphore {
     }
 }
 
-class AsyncQueue<T> {
-    queue = Array<T>();
+abstract class Queue<T> {
+    queue = Array<T>()
     semaphore = new AsyncSemaphore();
 
+    async abstract dequeue()
+    abstract enqueue(input: T)
+}
+
+
+class AsyncQueue<T> extends Queue<T> {
 
     async dequeue(): Promise<T> {
         await this.semaphore.wait()
@@ -35,15 +41,99 @@ class AsyncQueue<T> {
     }
 } 
 
+class Subscriber<T> {
+
+    constructor(public id: string, private queue: Queue<T>){}
+
+    pull(){
+        this.queue.dequeue().then(m => console.log(this.id + " processed this message: " + m))
+    }
+}
+
+class Publisher<T> {
+
+    constructor(public id: string, private queue: Queue<T>){}
+
+    push(input: T){
+        this.queue.enqueue(input)
+        console.log("Publisher " + this.id + " sent message: " + input)
+    }
+}
 
 
-setInterval(() => { }, 1000); // run program until explicit exit
 
-(async () => {
+// setInterval(() => { }, 1000); // run program until explicit exit
+
+// (async () => {
+//     const q = new AsyncQueue<number>()
+    
+//     const s1 = new Subscriber("s1", q)
+//     const s2 = new Subscriber("s2", q)
+
+//     const p1 = new Publisher("p1", q)
+//     const p2 = new Publisher("p2", q)
+//     const p3 = new Publisher("p3", q)
+
+//     p1.push(1111)
+//     s1.pull()
+//     s1.pull()
+//     s1.pull()
+//     p1.push(2222)
+//     p1.push(3334)
+
+//     for (let i = 0; i < 100; i += 1) {
+//         if (Math.random() > 0.5) {
+//             s1.pull()
+//         } else {
+//             p1.push(i)
+//         }
+//     }
+
+//    // process.exit()
+// })()
+
+const isArraySorted = require('is-array-sorted')
+
+async function testAsyncQueueBehavior(nOps: number): Promise<Boolean> {
+    const result = new Array<number>()
     const q = new AsyncQueue<number>()
-    q.dequeue().then(m => console.log(m))
-    q.dequeue().then(m => console.log(m))
-    q.enqueue(31)
-    q.enqueue(51)
-   // process.exit()
+
+    const enqueue = (m: number) => q.enqueue(m)
+    const dequeue = () => q.dequeue()
+    const promises = Array<Promise<void>>()
+
+    let enqueues = 0
+    let dequeues = 0
+
+    // Do a random permutation of enqueing and dequeing
+    for (let i = 0; i < nOps; i += 1) {
+        if (Math.random() > 0.5) {
+            enqueues += 1
+            // console.log(`${Date.now()} Enqueuing ${enqueues}`)
+            enqueue(enqueues)
+        } else {
+            dequeues += 1
+            // console.log(`${Date.now()} Dequeuing`)
+            promises.push(dequeue().then(v => { result.push(v) }))
+        }
+    }
+
+    // console.log(`Total enqueues ${enqueues}; dequeues ${dequeues}`)
+    const pending = Math.min(enqueues, dequeues)
+    await Promise.all(promises.slice(0, pending))
+
+    // Length should be equal minimum between enqueues and dequeues
+    const isLengthOk = pending === result.length 
+
+    // Messages should be ordered
+    const isSorted = isArraySorted(result)
+
+    return isLengthOk && isSorted
+}
+
+setInterval(() => { }, 1000); // run program forever until an explicit exit occurs
+(async () => {
+    const success = await testAsyncQueueBehavior(100)
+    console.log(success)
+    process.exit()
 })()
