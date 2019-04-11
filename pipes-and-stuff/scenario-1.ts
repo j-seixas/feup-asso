6c5056972209130501bae82fba6d24ea064372f8
João Seixas <up201505648@fe.up.pt>
2019-04-11 10:02:17 +0100

    scenario 1 working

--- pipes-and-stuff/scenario-1.ts
+++ pipes-and-stuff/scenario-1.ts
@@ -6,12 +6,10 @@ class AsyncSemaphore {
     }
 
     async wait(): Promise<void> {
-        return new Promise<void>(resolve => {
-            if(this.available > 0){
-                this.available--
-                resolve()
-            }
-        })
+        if(this.available == 0){
+            return new Promise<void>(resolve => resolve())
+        }
+        this.available--
 
     }
 }
@@ -22,10 +20,8 @@ class AsyncQueue<T> {
 
 
     async dequeue(): Promise<T> {
-        console.log("here\n");
         await this.semaphore.wait()
         return new Promise<T>(resolve => {
-            console.log("here 1\n");
             if(this.queue.length > 0)
                 resolve(this.queue.shift())
         })
@@ -34,13 +30,10 @@ class AsyncQueue<T> {
     }
 
     enqueue(input : T) {
-        console.log("here 2\n");
         this.queue.push(input)
         this.semaphore.signal()
-        console.log("here 3\n");
     }
 } 
-
 
 
 
@@ -48,55 +41,9 @@ setInterval(() => { }, 1000); // run program until explicit exit
 
 (async () => {
     const q = new AsyncQueue<number>()
-    q.enqueue(3)
     q.dequeue().then(m => console.log(m))
     q.dequeue().then(m => console.log(m))
-    q.enqueue(3)
-    process.exit()
+    q.enqueue(31)
+    q.enqueue(51)
+   // process.exit()
 })()
-
-// const isArraySorted = require('is-array-sorted')
-
-// async function testAsyncQueueBehavior(nOps: number): Promise<Boolean> {
-//     const result = new Array<number>()
-//     const q = new AsyncQueue<number>()
-
-//     const enqueue = (m: number) => q.enqueue(m)
-//     const dequeue = () => q.dequeue()
-//     const promises = Array<Promise<void>>()
-
-//     let enqueues = 0
-//     let dequeues = 0
-
-//     // Do a random permutation of enqueing and dequeing
-//     for (let i = 0; i < nOps; i += 1) {
-//         if (Math.random() > 0.5) {
-//             enqueues += 1
-//             // console.log(`${Date.now()} Enqueuing ${enqueues}`)
-//             enqueue(enqueues)
-//         } else {
-//             dequeues += 1
-//             // console.log(`${Date.now()} Dequeuing`)
-//             promises.push(dequeue().then(v => { result.push(v) }))
-//         }
-//     }
-
-//     // console.log(`Total enqueues ${enqueues}; dequeues ${dequeues}`)
-//     const pending = Math.min(enqueues, dequeues)
-//     await Promise.all(promises.slice(0, pending))
-
-//     // Length should be equal minimum between enqueues and dequeues
-//     const isLengthOk = pending === result.length 
-
-//     // Messages should be ordered
-//     const isSorted = isArraySorted(result)
-
-//     return isLengthOk && isSorted
-// }
-
-// setInterval(() => { }, 1000); // run program forever until an explicit exit occurs
-// (async () => {
-//     const success = await testAsyncQueueBehavior(100)
-//     console.log(success)
-//     process.exit()
-// })()
