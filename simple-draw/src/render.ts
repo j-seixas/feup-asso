e38d546331f75976bf324dfbacfdd0c75c018742
Sofia Silva <up201505628@fe.up.pt>
2019-05-15 21:00:11 +0100

    render circle svg

--- simple-draw/src/render.ts
+++ simple-draw/src/render.ts
@@ -15,11 +15,18 @@ export class SVGRender implements Render {
         for (const shape of objs) {
             if (shape instanceof Rectangle) {
                 const e = document.createElementNS("http://www.w3.org/2000/svg", "rect")
-                e.setAttribute('style', 'stroke: black; fill: white')
+                e.setAttribute('style', 'stroke: black; fill: transparent')
                 e.setAttribute('x', shape.x.toString())
                 e.setAttribute('y', shape.y.toString())
                 e.setAttribute('width', shape.width.toString())
                 e.setAttribute('height', shape.height.toString())
+                this.svg.appendChild(e)
+            } else if (shape instanceof Circle) {
+                const e = document.createElementNS('http://www.w3.org/2000/svg', 'circle')
+                e.setAttribute('style', 'stroke: black; fill: transparent')
+                e.setAttribute('cx', shape.x.toString())
+                e.setAttribute('cy', shape.y.toString())
+                e.setAttribute('r', shape.radius.toString())
                 this.svg.appendChild(e)
             }
         }
@@ -40,7 +47,7 @@ export class CanvasRender implements Render {
                 this.ctx.ellipse(shape.x, shape.y, shape.radius, shape.radius, 0, 0, 2 * Math.PI)
                 this.ctx.stroke()
             } else if (shape instanceof Rectangle) {
-                this.ctx.strokeRect(shape.x, shape.y, shape.width, shape.height)   
+                this.ctx.strokeRect(shape.x, shape.y, shape.width, shape.height)
             }
         }
     }
